f7317387102935c68319ed8b828beb9039495ab2
Sophie Grape <sophie.grape@physics.uu.se>
2019-03-07 17:32:50 +0100

    Add files via upload

---
+++ Test/simple_math.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  7 15:16:22 2019
+@author: soohl254
+A collection of simple math operations.  
+"""
+
+
+def simple_add(a,b):
+"""
+Simple addition of a and b.  
+
+Parameters
+----------
+a : object supporting addition
+b : object supporting addition
+
+Examples
+--------
+c=simple_add(a,b)
+"""
+    return a+b
+
+def simple_sub(a,b):
+    return a-b
+
+def simple_mult(a,b):
+    return a*b
+
+def simple_div(a,b):
+    return a/b
+
+def poly_first(x, a0, a1):
+    return a0 + a1*x
+
+def poly_second(x, a0, a1, a2):
+    return poly_first(x, a0, a1) + a2*(x**2)
+
+# Feel free to expand this list with more interesting mathematical operations...
+# .....
---
+++ Test/source/index.rst
@@ -0,0 +1,23 @@
+.. MathamathicalOperations documentation master file, created by
+   sphinx-quickstart on Thu Mar  7 15:42:38 2019.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to MathamathicalOperations's documentation!
+===================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+   
+.. automodule:: fib
+   :members:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
